b3db5281af3f85be01901578b90bea039390c222
Ronald Arias <ariasron@hotmail.com>
2022-04-13 20:11:19 -0500

    simple example of transform the code from the input using esbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,44 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useRef } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useState } from 'react'
+import * as esbuild from 'esbuild-wasm'
 
 const App = () => {
-  return <h1>hi</h1>
+  const ref = useRef<any>()
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    })
+  }
+
+  useEffect(() => {
+    startService()
+  }, [])
+
+  const onClick = async () => {
+    if(!ref.current) return
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015'
+    })
+
+    setCode(result.code)
+  }
+
+  return (
+    <div>
+      <textarea onChange={e => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+        </div>
+      <pre>{code}</pre>
+    </div>
+  )
 }
 
 const rootElement = document.getElementById('root')
