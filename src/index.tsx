33b8a470b85a939f6550b4f6b8d9ee53dc152d04
Ronald Arias <ariasron@hotmail.com>
2022-04-14 15:47:54 -0500

    creates a plugin file to use esbuild as a bundle in the browser

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { StrictMode, useEffect, useRef } from 'react'
 import { createRoot } from 'react-dom/client'
 import { useState } from 'react'
 import * as esbuild from 'esbuild-wasm'
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 
 const App = () => {
   const ref = useRef<any>()
@@ -22,12 +23,16 @@ const App = () => {
   const onClick = async () => {
     if(!ref.current) return
 
-    const result = await ref.current.transform(input, {
-      loader: 'jsx',
-      target: 'es2015'
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
     })
 
-    setCode(result.code)
+    console.log(result)
+
+    setCode(result.outputFiles[0].text)
   }
 
   return (
