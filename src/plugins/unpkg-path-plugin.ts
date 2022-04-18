3928256c92c08ef80d9e9b6cb7171ea1abcd4b4a
Ronald Arias <ariasron@hotmail.com>
2022-04-18 12:44:15 -0500

    dynamically fetching modules, the code is still hardcoded but it works

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,4 +1,5 @@
 import * as esbuild from 'esbuild-wasm'
+import axios from 'axios'
 
 export const unpkgPathPlugin = () => {
   return {
@@ -6,7 +7,14 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args)
-        return { path: args.path, namespace: 'a' }
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' }
+        } else if (args.path === 'tiny-test-pkg') {
+          return {
+            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+            namespace: 'a',
+          }
+        }
       })
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,15 +24,15 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from './message';
+              const message = require('tiny-test-pkg');
               console.log(message);
             `,
           }
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
-          }
+        }
+        const { data } = await axios.get(args.path)
+        return {
+          loader: 'jsx',
+          contents: data,
         }
       })
     },
