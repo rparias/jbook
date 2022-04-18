ba2fd6c0aa6d651bca2a7a1dbff2eaa5ef5a2a25
Ronald Arias <ariasron@hotmail.com>
2022-04-18 12:48:57 -0500

    generates automatically the unpkg url with the properly path

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -9,11 +9,11 @@ export const unpkgPathPlugin = () => {
         console.log('onResolve', args)
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' }
-        } else if (args.path === 'tiny-test-pkg') {
-          return {
-            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-            namespace: 'a',
-          }
+        }
+
+        return {
+          path: `https://unpkg.com/${args.path}`,
+          namespace: 'a',
         }
       })
 
