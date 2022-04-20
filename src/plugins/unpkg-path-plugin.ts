2e96f0fc325436b35985193896871c2e7b26c5f2
Ronald Arias <ariasron@hotmail.com>
2022-04-19 20:43:23 -0500

    resolves nested paths using resolveDir and URL consructor

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -13,7 +13,8 @@ export const unpkgPathPlugin = () => {
 
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
-            path: new URL(args.path, `${args.importer}/`).href,
+            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
+              .href,
             namespace: 'a',
           }
         }
@@ -31,15 +32,16 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           }
         }
-        const { data } = await axios.get(args.path)
+        const { data, request } = await axios.get(args.path)
         return {
           loader: 'jsx',
           contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
         }
       })
     },
